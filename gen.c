6c4cd498d5e539a03be6867cb9ed91395d360e42
msgr0 <mttsgro@gmail.com>
2025-04-23 14:41:29 +0900

    malloc fixes, printing with fwrite

--- gen.c
+++ gen.c
@@ -24,68 +24,35 @@ int main(int argc, char *argv[]) {
 
     size_t padding = cols % 8;
     size_t nw = (cols / 8) + (padding > 0);
-    size_t fcols = cols + padding;
+    size_t fcols = cols + 8 - padding;
 
-    char *frow = (char *)malloc(fcols + sizeof(char));
+    char *frow = (char *)malloc(fcols * sizeof(char));
 
     for (size_t i = 0; i < cols; i++) {
       frow[i] = (rand() & 1U);
       //printf("%c", frow[i] + 48);
     }
-    for (size_t i = 0; i < padding; i++) {
+    for (size_t i = 0; i < 8 - padding; i++) {
       frow[i + cols] = 0;
     }
     //printf("b-nw:%zu-b\n", nw);
 
-    char *ftcol_pack = (char *)malloc(rows + sizeof(char));
+    char *ftcol_pack = (char *)malloc(rows * sizeof(char));
     size_t b = 0;
     for (size_t j = 0; j < nw; j++) { // per ogni w-sized colonna
       for (size_t i = 0; i < rows; i++) { // per ogni riga
         for (size_t b = 8; b >0; b--) { // per ogni bit della w-sized colonna
           ftcol_pack[i] = (ftcol_pack[i] << 1) | ( rand() > (int)(err * INT32_MAX) ? (frow[(j)*8 + b-1] & 1U) : (rand() & 1U) );
         }
-        putc(ftcol_pack[i], stdout);
       }
+      
+      fwrite(ftcol_pack, sizeof(char), rows, stdout);
+      
       puts("");
     }
 
-
-//    char *frow_pack = (char *)malloc(cols + sizeof(char));
-//    size_t b = 0;
-//    for (size_t i = 0; i < cols; i++) {
-//      // printf("curr i %zu\n", i);
-//
-//      for (size_t j = 0; j < rows; j++) {
-//        if (rand() > (int)(err * INT32_MAX)) {
-//          frow_pack[j] = ((frow[j] & 1U) << j) | (frow_pack[j]);
-//        } else {
-//          frow_pack[j] = ((rand() & 1U) << j) | (frow_pack[j]);
-//        }
-//      }
-//
-//      if (b % 8 == 7) {
-//        // printf("printing encoded %zu \n", (i*8)+b);
-//        for (size_t j = 0; j < cols; j++) {
-//          // printf("%c", frow_pack[j]);
-//          putc(frow_pack[j], stdout);
-//        }
-//        puts("");
-//      }
-//      b++;
-//    }
-//
-//    if (b % 8 != 0) {
-//      for (size_t j = 0; j < cols; j++) {
-//        putc(frow_pack[j], stdout);
-//      }
-//      puts("");
-//    }
-//    // for (size_t j = 0; j < cols; j++) {
-//    //   printf("%c", frow[j] + 48);
-//    // }
-//    // puts("");
-//    free(frow_pack);
-//    free(frow);
+    free(frow);
+    free(ftcol_pack);
 
     return EXIT_SUCCESS;
   }
